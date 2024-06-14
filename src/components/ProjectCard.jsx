78be88e4ba0fb89255710e37ed2821978bdafb99
Joe <jakatsa5@gmail.com>
2024-06-14 12:29:25 +0300

    project card updates

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import "../styles/Home.css";
+
 export const ProjectCard = ({ title, description, liveLink, githubLink }) => {
   return (
     <div className="col-lg-11 col-xl-9 col-xxl-8">
@@ -8,24 +9,24 @@ export const ProjectCard = ({ title, description, liveLink, githubLink }) => {
         <div className="card-body p-0">
           <div className="d-flex align-items-center">
             <div className="p-5">
-              <Link to={liveLink}>
+              <a href={liveLink} target="_blank" rel="noopener noreferrer">
                 <h3
                   className="fw-bolder text-gradient"
                   style={{ color: "black", textDecoration: "none" }}
                 >
                   {title}
                 </h3>
-              </Link>
+              </a>
               <p>
                 {description}
-                <Link to={githubLink}>
+                <a href={githubLink} target="_blank" rel="noopener noreferrer">
                   <h4
                     className="fw-bolder"
                     style={{ color: "black", textDecoration: "none" }}
                   >
                     Github link
                   </h4>
-                </Link>
+                </a>
               </p>
             </div>
           </div>
